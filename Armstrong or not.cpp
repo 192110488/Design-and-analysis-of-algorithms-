eeab41cafc7d84cf2a64a6c01b7dd59a61898ec6
subhash varma <112234427+192110488@users.noreply.github.com>
2023-08-09 10:47:41 +0530

    Update Armstrong or not.cpp

--- Armstrong or not.cpp
+++ Armstrong or not.cpp
@@ -1,10 +1,13 @@
-#include<stdio.h>
+ #include<stdio.h>
+#include<ctype.h>
 int main()
 {
 	int n,temp,r,sum=0;
 	printf("enter the number::");
 	scanf("%d",&n);
 	temp=n;
+	if(isdigit(n))
+	{
 	while(n!=0)
 	{
 		r=n%10;
@@ -19,4 +22,7 @@ int main()
 	{
 		printf("The given number is Not Armstrong number.");
 	}
+	}
+	else
+		printf("invalid input.");
 }
