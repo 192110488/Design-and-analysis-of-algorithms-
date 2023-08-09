79a0563cda836fccd109a59be79eb51da6c01d5d
subhash varma <112234427+192110488@users.noreply.github.com>
2023-08-09 23:11:51 +0530

    Update merge sort.cpp

--- merge sort.cpp
+++ merge sort.cpp
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 int main()
 {
-    int i,n,j,k,temp,m;
-    printf("Enter the size of the first array: ");
-    scanf("%d", &n);
-    printf("Enter the size of the second array: ");
-    scanf("%d", &k);
-    int a[n],b[k];
-    printf("Enter the elements of the first array: \n");
-    for (i = 0; i < n; i++)
-    	scanf("%d", &a[i]);
-    for(i=0;i<n;i++){
+	int i,n,j,k,temp,m;
+    	printf("Enter the size of the first array: ");
+    	scanf("%d", &n);
+    	printf("Enter the size of the second array: ");
+    	scanf("%d", &k);
+    	int a[n],b[k];
+    	printf("Enter the elements of the first array: \n");
+    	for (i = 0; i < n; i++)
+    		scanf("%d", &a[i]);
+    	for(i=0;i<n;i++){
 		for(j=i+1;j<n;j++){
 			if(a[i]>a[j])
 			{
@@ -21,9 +21,9 @@ int main()
 			}}}
     
 	printf("Enter the elements of the second array: \n");
-    for (j = 0; j < k; j++)
-    	scanf("%d", &b[j]);
-    for(i=0;i<k;i++){
+    	for (j = 0; j < k; j++)
+    		scanf("%d", &b[j]);
+    	for(i=0;i<k;i++){
 		for(j=i+1;j<k;j++){
 			if(b[i]>b[j])
 			{
@@ -33,10 +33,10 @@ int main()
 			}}}
     
 	int c[n+k];
-    i = j = 0;
-    for (m=0;m<n+k;m++)
-    {
-    	if(i<n && j<k)
+    	i = j = 0;
+    	for (m=0;m<n+k;m++)
+    	{
+    		if(i<n && j<k)
 		{
 			if(a[i]<b[j])
 			{
@@ -50,15 +50,15 @@ int main()
 			}
 		}
 		else if (i < n)
-        {
-            c[m] = a[i];
-            i++;
-        }
-        else
-        {
-            c[m] = b[j];
-            j++;
-        }
+        	{
+            		c[m] = a[i];
+            		i++;
+        	}
+        	else
+        	{
+            		c[m] = b[j];
+            		j++;
+        	}
 	}
     printf("The merged array is: \n");
     for (m=0;m<n+k;m++)
@@ -66,5 +66,4 @@ int main()
         printf("%d ", c[m]);
     }
     printf("\n");
-    return 0;
 }
