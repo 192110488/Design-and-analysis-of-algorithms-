56587c3bce285ac2dd953956e40522a2d7bdcacd
subhash varma <112234427+192110488@users.noreply.github.com>
2023-08-09 19:03:53 +0530

    Update Binary search algorithm.cpp

--- Binary search algorithm.cpp
+++ Binary search algorithm.cpp
@@ -1,40 +1,48 @@
-#include<stdio.h>
+ #include <stdio.h>  
+int Search(int a[], int beg, int end, int t)    
+{    
+    int mid;    
+    if(end >= beg)     
+    {        mid = (beg + end)/2;    
+  
+        if(a[mid] == t)    
+        {                 
+            return mid+1;    
+        }      
+        else if(a[mid] < t)     
+        {  
+            return Search(a, mid+1, end, t);    
+        }     
+        else     
+        {  
+            return Search(a, beg, mid-1, t);    
+        }          
+    }    
+    return -1;     
+}   
 int main()
-{
-	int m,n=0,s,a[20],temp,i,j;
-	printf("enter the noof elements");
-	scanf("%d",&s);
-	printf("enter the target elements:");
-	scanf("%d",&m);
-	printf("enter the array elements:");
-	for(i=0;i<s;i++)
+{  
+	int res,i,t,n,a[30];
+	printf("number of elements you are going to enter ::");
+	scanf("%d",&n);
+	printf("enter the required target number::");
+	scanf("%d",&t);
+	printf("enter the numbers:");
+	for(i=0;i<n;i++)
 	{
 		scanf("%d",&a[i]);
-	}
-	for(i=0;i<s;i++)
-	{
-		for(j=i+1;j<s;j++)
-		{
-			if(a[i]>a[j])
-			{
-				temp=a[i];
-				a[i]=a[j];
-				a[j]=temp;
-			}
+	}    
+  	res = Search(a, 0, n-1, t);
+ 	printf("The elements of the array are - ");  
+  	for (i=0;i<n;i++)  
+ 		{
+  			printf("%d ", a[i]);
 		}
-	}
-	for(i=0;i<s;i++)
-	{
-		if(a[i]==m)
-		{
-			n=1;
-			printf("Target found at position a[%d]",i);
-			break;
-		}
-		else continue;
-	}
-	if(n==1)
-		printf("\n successful.");
-	else
-		printf("Target not found.");
-}
+  	printf("\nElement to be searched is - %d", t);  
+  	if (res == -1)  
+  		printf("\nElement is not present in the array");  
+  	else  
+  		printf("\nElement is present at %d position of array", res);  
+  	return 0;  
+}  
+ 
